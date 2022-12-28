8fb7ec8363bd2286a465505f057dad2701860273
neslihan-volvo <117173037+neslihan-volvo@users.noreply.github.com>
2022-12-28 16:19:59 +0100

    constraints are added

--- MusicSearchUIKit_NoStoryboards/DetailViewController.swift
+++ MusicSearchUIKit_NoStoryboards/DetailViewController.swift
@@ -38,6 +38,7 @@ class DetailViewController: UIViewController {
         view.addSubview(trackName)
         view.addSubview(collectionName)
         view.addSubview(playButton)
+        setConstraints()
         view.subviews.forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
         }
@@ -50,6 +51,18 @@ class DetailViewController: UIViewController {
             print(error)
         }
         return image
+    }
+    func setConstraints() {
+        musicImage.topAnchor.constraint(equalTo: self.view.topAnchor, constant: UIScreen.main.bounds.height * 0.2).isActive = true
+        musicImage.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: UIScreen.main.bounds.width * 0.35).isActive = true
+        artistName.topAnchor.constraint(equalTo: musicImage.topAnchor, constant: 150).isActive = true
+        artistName.leftAnchor.constraint(equalTo: musicImage.leftAnchor, constant:0).isActive = true
+        trackName.topAnchor.constraint(equalTo: artistName.topAnchor, constant: 25).isActive = true
+        trackName.leftAnchor.constraint(equalTo: artistName.leftAnchor, constant:0).isActive = true
+        collectionName.topAnchor.constraint(equalTo: trackName.topAnchor, constant: 25).isActive = true
+        collectionName.leftAnchor.constraint(equalTo: trackName.leftAnchor, constant:0).isActive = true
+        playButton.topAnchor.constraint(equalTo: collectionName.topAnchor, constant: 25).isActive = true
+        playButton.leftAnchor.constraint(equalTo: collectionName.leftAnchor, constant:0).isActive = true
     }
 }
 
