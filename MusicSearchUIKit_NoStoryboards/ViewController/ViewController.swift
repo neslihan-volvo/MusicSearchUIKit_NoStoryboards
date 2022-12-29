e901a56dccbd9d4cdf260cc7ce297c3559582057
neslihan-volvo <117173037+neslihan-volvo@users.noreply.github.com>
2022-12-29 11:43:00 +0100

    refactoring

--- MusicSearchUIKit_NoStoryboards/ViewController/ViewController.swift
+++ MusicSearchUIKit_NoStoryboards/ViewController/ViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class ViewController: UIViewController {
 
     private let viewModel = ContentViewModel()
     private let imageDownloader = ImageDownloader()
@@ -33,29 +33,15 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         super.viewDidLayoutSubviews()
         collectionView.frame = view.bounds
     }
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return viewModel.state.results?.count ?? 0
+}
+
+extension ViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        let searchKey = searchController.searchBar
+        viewModel.keyword = searchKey.text!
     }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MusicCollectionViewCell.identifier, for: indexPath) as! MusicCollectionViewCell
-        guard let musicItem = viewModel.state.results?[indexPath.row] else {
-            fatalError("Oh no should not happen")
-        }
-        cell.artistName.text = musicItem.artistName
-        cell.trackName.text = musicItem.trackName
-        imageTasks[indexPath]?.cancel()
-        let myTask = Task {
-            
-            @MainActor in
-            cell.musicImage.image = await getImage(url: musicItem.artworkUrl60)
-        }
-        imageTasks[indexPath] = myTask
-        return cell
-    }
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: (view.frame.size.width-16), height: 60)
-    }
+}
+extension ViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         guard let musicItem = viewModel.state.results?[indexPath.row] else {
             fatalError("Oh no should not happen")
@@ -65,21 +51,35 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         //self.present(detailViewController, animated: true)
         navigationController?.pushViewController(detailViewController, animated: true)
     }
-    func getImage(url: String) async -> UIImage {
-        var image = UIImage()
-        do {
-            image = try await imageDownloader.getImage(url: url)
-        } catch {
-            print("get image resulted error")
+    
+}
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.state.results?.count ?? 0
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MusicCollectionViewCell.identifier, for: indexPath) as! MusicCollectionViewCell
+        guard let musicItem = viewModel.state.results?[indexPath.row] else {
+            fatalError("Oh no should not happen")
         }
-        return image
+        cell.artistName.text = musicItem.artistName
+        cell.trackName.text = musicItem.trackName
+        imageTasks[indexPath]?.cancel()
+        let myTask = Task {
+            @MainActor in
+            do {
+                cell.musicImage.image = try await imageDownloader.getImage(url: musicItem.artworkUrl60)
+            } catch {
+                print("get image resulted error")
+            }
+        }
+        imageTasks[indexPath] = myTask
+        return cell
     }
 }
-extension ViewController: UISearchResultsUpdating {
-    func updateSearchResults(for searchController: UISearchController) {
-        let searchKey = searchController.searchBar
-        viewModel.keyword = searchKey.text!
-    
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: (view.frame.size.width-16), height: 60)
     }
 }
 
